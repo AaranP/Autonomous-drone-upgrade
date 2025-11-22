b6ef2884349cee267768fe9db043de3990e5c114
aaran <aaranp1919@gmail.com>
2025-11-21 21:31:29 -0800

    single sh file for the test mapping part of chapter 10

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -3,4 +3,5 @@ roslaunch realsense2_camera rs_camera.launch & sleep 10;
 roslaunch mavros px4.launch & sleep 10;
 roslaunch vins fast_drone_250.launch & sleep 10;
 echo "All processes started."
-wait;
+sleep 30;
+echo "Can run roslaunch now"
---
+++ shfiles/testmap.sh
@@ -0,0 +1,2 @@
+sh shfiles/rspx4.sh
+roslaunch ego_planner single_run_in_exp.launch
