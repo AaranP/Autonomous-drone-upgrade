3576f20831a1afcd2ce6286218aad3f3f30ae7e8
aaran <aaranp1919@gmail.com>
2025-11-21 23:54:40 -0800

    Revert "single sh file for the test mapping part of chapter 10"
    This reverts commit b6ef2884349cee267768fe9db043de3990e5c114.

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -3,5 +3,4 @@ roslaunch realsense2_camera rs_camera.launch & sleep 10;
 roslaunch mavros px4.launch & sleep 10;
 roslaunch vins fast_drone_250.launch & sleep 10;
 echo "All processes started."
-sleep 30;
-echo "Can run roslaunch now"
+wait;
--- shfiles/testmap.sh
+++
@@ -1,2 +0,0 @@
-sh shfiles/rspx4.sh
-roslaunch ego_planner single_run_in_exp.launch
