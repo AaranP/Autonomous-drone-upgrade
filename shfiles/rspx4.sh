956095f53894cccc8a7674f4f0f57ea06329ab90
aaran <aaranp1919@gmail.com>
2025-11-30 21:16:52 -0800

    switching the launch order of module

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -2,14 +2,17 @@
 
 sudo chmod 777 /dev/ttyACM0 & sleep 2;
 
+echo "--- Starting RealSense Camera ---"
+# Lower the framerate to reduce power and bandwidth demand
+roslaunch realsense2_camera rs_camera.launch 
+#depth_width:=640 depth_height:=480 depth_fps:=15 infra_width:=640 infra_height:=480 infra_fps:=15 &
+sleep 20; # Give camera time to start publishing
+
+
 echo "--- Starting MAVROS ---"
 roslaunch mavros px4.launch &
 sleep 20; # Give MAVROS time to initialize and connect to FCU
 
-echo "--- Starting RealSense Camera ---"
-# Lower the framerate to reduce power and bandwidth demand
-roslaunch realsense2_camera rs_camera.launch depth_width:=640 depth_height:=480 depth_fps:=15 infra_width:=640 infra_height:=480 infra_fps:=15 &
-sleep 20; # Give camera time to start publishing
 
 echo "--- Starting VINS-Fusion ---"
 mkdir -p /root/vins_output/pose_graph # Ensure the output directories exist
