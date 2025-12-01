fe71641d837272e036eeb58d5e1335624ac425ca
AaranP <aaranp1919@gmail.com>
2025-11-30 16:01:33 -0800

    YAML error from the first line

--- run_container.sh
+++ run_container.sh
@@ -102,8 +102,8 @@ docker run -it --rm \
     -e DRONE_ROS_IP="${DRONE_ROS_IP}" \
     -v /dev:/dev \
     -v "$(pwd)/src/fastdrone/config:/root/catkin_ws/src/fastdrone/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/config:/root/catkin_ws/src/VINS-Fusion/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/vins_estimator/launch:/root/catkin_ws/src/VINS-Fusion/vins_estimator/launch" \
+    -v "$(pwd)/src/realflight_modules/VINS-Fusion/config:/root/catkin_ws/src/fastdrone/src/realflight_modules/VINS-Fusion/config" \
+    -v "$(pwd)/src/realflight_modules/VINS-Fusion/vins_estimator/launch:/root/catkin_ws/src/fastdrone/src/realflight_modules/VINS-Fusion/vins_estimator/launch" \
     -v "$(pwd)/src/planner/plan_manage/launch:/root/catkin_ws/src/planner/plan_manage/launch" \
     -v "$(pwd)/shfiles:/root/shfiles" \
     -v "$(pwd)/vins_output:/root/vins_output" \
