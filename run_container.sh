45c6052aeb3adb6f50c42195994a03624d5dc70f
ethansmith12 <ehmsmith12@gmail.com>
2025-12-02 00:05:32 -0800

    fix run container2

--- run_container.sh
+++ run_container.sh
@@ -110,7 +110,6 @@ docker run -it --rm \
     -v "$(pwd)/shfiles:/root/shfiles" \
     -v "$(pwd)/vins_output:/root/vins_output" \
     -v "$(pwd)/src/realflight_modules/realsense-ros/realsense2_camera/launch:/root/catkin_ws/src/realflight_modules/realsense-ros/realsense2_camera/launch" \
-
     fastdrone_image_pi:latest-arm64 \
     /root/shfiles/server.sh # Execute the server setup script
     
