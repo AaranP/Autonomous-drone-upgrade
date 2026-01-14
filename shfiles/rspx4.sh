cea8264a3b23ebe24b3bed69f0e505fe86f7238e
aaran <aaranp1919@gmail.com>
2026-01-13 18:14:31 -0800

    added fdilink for launch file

--- shfiles/rspx4.sh
+++ shfiles/rspx4.sh
@@ -4,16 +4,15 @@ sudo chmod 777 /dev/ttyACM0 & sleep 2;
 
 echo "--- Starting RealSense Camera ---"
 # Lower the framerate to reduce power and bandwidth demand
-sleep 5;
-roslaunch fdilink_ahrs ahrs_data.launch & sleep 5
 roslaunch realsense2_camera rs_camera.launch 
-sleep 20; # Give camera time to start publishing
+sleep 5; # Give camera time to start publishing
 
 
 echo "--- Starting MAVROS ---"
+roslaunch fdilink_ahrs ahrs_data.launch & sleep 5
 
-roslaunch mavros px4.launch &
-sleep 20; # Give MAVROS time to initialize and connect to FCU
+#roslaunch mavros px4.launch &
+sleep 5; # Give MAVROS time to initialize and connect to FCU
 
 
 echo "--- Starting VINS-Fusion ---"
@@ -21,7 +20,7 @@ mkdir -p /root/vins_output/pose_graph # Ensure the output directories exist
 chmod -R 777 /root/vins_output
 
 roslaunch vins fast_drone_250.launch &
-sleep 20; # Give VINS time to start processing
+sleep 10; # Give VINS time to start processing
 
 echo "All processes started."
 wait;
