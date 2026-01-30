56e000bbb6ce1d9872bfc89ddd3f926efec61d11
aaran <aaranp1919@gmail.com>
2026-01-29 20:31:43 -0800

    Fixed file path in docker and simplified it

--- run_container.sh
+++ run_container.sh
@@ -103,16 +103,11 @@ docker run -it --rm \
     -e ROS_MASTER_URI="http://${DRONE_ROS_IP}:11311" \
     -e ROS_IP="${DRONE_ROS_IP}" \
     -v /dev:/dev \
-    -v "$(pwd)/src/fastdrone/config:/root/catkin_ws/src/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/config:/root/catkin_ws/src/realflight_modules/VINS-Fusion/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/vins_estimator/launch:/root/catkin_ws/src/realflight_modules/VINS-Fusion/vins_estimator/launch" \
-    -v "$(pwd)/src/fdilink_ahrs:/root/catkin_ws/src/fdilink_ahrs" \
-    -v "$(pwd)/shfiles:/root/shfiles" \
+    -v "$(pwd)/src:/root/catkin_ws/src" \
+    -v "$(pwd)/shfiles:/root/catkin_ws/shfiles" \
     -v "$(pwd)/vins_output:/root/vins_output" \
-    -v "$(pwd)/src/realflight_modules/realsense-ros/realsense2_camera/launch:/root/catkin_ws/src/realflight_modules/realsense-ros/realsense2_camera/launch" \
-    -v "$(pwd)/src/fuel_planner:/root/catkin_ws/src/fuel_planner" \
     fastdrone_image_pi:latest-arm64 \
-    /root/shfiles/server.sh # Execute the server setup script
+    /root/catkin_ws/shfiles/server.sh # Execute the server setup script
     
 #Opens another terminal in the docker session (this line will only run if the above docker run command exits)
 #docker exec -it fast_drone_container /bin/bash
