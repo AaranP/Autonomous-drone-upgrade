0445d15fa361961b4b6665d3c55f9d185574a044
aaran <aaranp1919@gmail.com>
2026-01-29 20:23:01 -0800

    Fixed file path in docker

--- run_container.sh
+++ run_container.sh
@@ -103,9 +103,9 @@ docker run -it --rm \
     -e ROS_MASTER_URI="http://${DRONE_ROS_IP}:11311" \
     -e ROS_IP="${DRONE_ROS_IP}" \
     -v /dev:/dev \
-    -v "$(pwd)/src/fastdrone/config:/root/catkin_ws/src/fastdrone/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/config:/root/catkin_ws/src/fastdrone/src/realflight_modules/VINS-Fusion/config" \
-    -v "$(pwd)/src/realflight_modules/VINS-Fusion/vins_estimator/launch:/root/catkin_ws/src/fastdrone/src/realflight_modules/VINS-Fusion/vins_estimator/launch" \
+    -v "$(pwd)/src/fastdrone/config:/root/catkin_ws/src/config" \
+    -v "$(pwd)/src/realflight_modules/VINS-Fusion/config:/root/catkin_ws/src/realflight_modules/VINS-Fusion/config" \
+    -v "$(pwd)/src/realflight_modules/VINS-Fusion/vins_estimator/launch:/root/catkin_ws/src/realflight_modules/VINS-Fusion/vins_estimator/launch" \
     -v "$(pwd)/src/fdilink_ahrs:/root/catkin_ws/src/fdilink_ahrs" \
     -v "$(pwd)/shfiles:/root/shfiles" \
     -v "$(pwd)/vins_output:/root/vins_output" \
