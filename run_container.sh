a7d200783c884982aabb9c32f648af8d49c663b6
danielj142 <danieljo142@gmail.com>
2026-02-02 21:21:24 -0800

    fix pwd error

--- run_container.sh
+++ run_container.sh
@@ -110,7 +110,7 @@ docker run -it --rm \
     -v "$(pwd)/shfiles:/root/shfiles" \
     -v "$(pwd)/vins_output:/root/vins_output" \
     -v "$(pwd)/src/realflight_modules/realsense-ros/realsense2_camera/launch:/root/catkin_ws/src/realflight_modules/realsense-ros/realsense2_camera/launch" \
-    -v "$(pwd)/src/utils/odom_interpolator/launch:/root/catkin_ws/src/utils/odom_interpolator/launch" \
+    -v "$(pwd)/src/utils/odom_interpolator:/root/catkin_ws/src/utils/odom_interpolator" \
     fastdrone_image_pi:latest-arm64 \
     /root/shfiles/server.sh # Execute the server setup script
     
