f8700071c9a334d5ab2494c1b6d710a1b51f7c63
ethansmith12 <ehmsmith12@gmail.com>
2026-03-21 17:29:46 -0700

    added vins to launch_client.sh

--- shfiles/launch_client.sh
+++ shfiles/launch_client.sh
@@ -1,4 +1,12 @@
-# Launch VINS and FUEL in parallel
+# Launch VINS and then FUEL
+
+echo "--- Starting VINS-Fusion ---"
+mkdir -p /root/vins_output/pose_graph # Ensure the output directories exist
+chmod -R 777 /root/vins_output
+
+roslaunch vins fast_drone_250.launch &
+sleep 10; # Give VINS time to start processing
+
 echo "--- Starting FUEL ---"
 
 # 1. Decompress from the standard RealSense compressed topic
