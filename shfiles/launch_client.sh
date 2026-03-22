2384cdeda2197b5a71e73a2fb7eaba4a3707a5ed
danielj142 <danieljo142@gmail.com>
2026-03-22 16:03:17 -0700

    try and compress images

--- shfiles/launch_client.sh
+++ shfiles/launch_client.sh
@@ -1,5 +1,15 @@
 # Launch VINS and then FUEL
 
+#sync clocks
+sudo systemctl restart chrony
+sleep 2
+
+rosrun image_transport republish compressesd in:=/camera/infra1/image_rect_raw out:=/camera/infra1/image_rect_raw_decompressed &
+sleep 1
+rosrun image_transport republish compressesd in:=/camera/infra2/image_rect_raw out:=/camera/infra2/image_rect_raw &
+sleep 1
+
+
 echo "--- Starting VINS-Fusion ---"
 mkdir -p /root/vins_output/pose_graph # Ensure the output directories exist
 chmod -R 777 /root/vins_output
@@ -14,4 +24,4 @@ rosrun image_transport republish compressedDepth in:=/camera/depth/image_rect_ra
 sleep 2
 
 # 2. Launch exploration, telling it to use the DECOMPRESSED topic
-roslaunch exploration_manager exploration.launch 
+roslaunch exploration_manager exploration.launch
